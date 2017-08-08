b1a708d49cae4e9a485d70d8c476a09ce585ab00
mayyan <mayyan@yahoo.com>
2017-08-08 15:23:53 -0700

    apply webpack

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # myapp
+
+install dependencies:
+    $ cd myapp && npm install
+
+build:
+	$ npm run build
+
+run the app:
+    $ DEBUG=myapp:* npm start
--- app.js
+++ app.js
@@ -23,8 +23,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(lessMiddleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(lessMiddleware(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('/', index);
 app.use('/users', users);
